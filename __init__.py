7550613b2af212583f8a03e04c65cd8d13aadf4a
Cliff Hults <Cliff.hults@gmail.com>
2017-09-18 20:24:13 -0400

    overhauled light brightness handling

--- __init__.py
+++ __init__.py
@@ -41,7 +41,6 @@ class HomeAssistantClient(object):
             for state in req.json():
                 try:
                     if state['entity_id'].split(".")[0] in types:
-                        LOGGER.debug("Entity Data: %s" % state)
                         score = fuzz.ratio(
                             entity,
                             state['attributes']['friendly_name'].lower())
@@ -60,6 +59,7 @@ class HomeAssistantClient(object):
     #
 
     def find_entity_attr(self, entity):
+        LOGGER.debug("!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#")
         if self.ssl:
             req = get("%s/api/states" %
                       self.url, headers=self.headers, verify=self.verify)
@@ -69,17 +69,23 @@ class HomeAssistantClient(object):
         if req.status_code == 200:
             for attr in req.json():
                 if attr['entity_id'] == entity:
-                    try:
-                        unit_measur = attr['attributes']['unit_of_measurement']
-                        sensor_name = attr['attributes']['friendly_name']
+                    entity_attrs = attr['attributes']
+                    if attr['entity_id'].startswith('light.'):
+                        unit_measur = entity_attrs['brightness']
+                        sensor_name = entity_attrs['friendly_name']
                         sensor_state = attr['state']
-                        return unit_measure, sensor_name, sensor_state
-                    except BaseException:
-                        unit_measur = 'null'
-                        sensor_name = attr['attributes']['friendly_name']
-                        sensor_state = attr['state']
-                        return unit_measure, sensor_name, sensor_state
-
+                        return unit_measur, sensor_name, sensor_state
+                    else:
+                        try:
+                            unit_measur = entity_attrs['unit_of_measurement']
+                            sensor_name = entity_attrs['friendly_name']
+                            sensor_state = attr['state']
+                            return unit_measur, sensor_name, sensor_state
+                        except BaseException:
+                            unit_measur = 'null'
+                            sensor_name = entity_attrs['friendly_name']
+                            sensor_state = attr['state']
+                            return unit_measur, sensor_name, sensor_state
         return None
 
     def execute_service(self, domain, service, data):
@@ -109,16 +115,29 @@ class HomeAssistantSkill(MycroftSkill):
         self.language = self.config_core.get('lang')
         self.load_vocab_files(join(dirname(__file__), 'vocab', self.lang))
         self.load_regex_files(join(dirname(__file__), 'regex', self.lang))
-        self.__build_lighting_intent()
+        self.__build_switch_intent()
+        self.__build_light_set_intent()
+        self.__build_light_adjust_intent()
         self.__build_automation_intent()
         self.__build_sensor_intent()
         self.__build_tracker_intent()
 
-    def __build_lighting_intent(self):
-        intent = IntentBuilder("LightingIntent").require(
-            "LightActionKeyword").require("Action").require("Entity").build()
-        # TODO - Locks, Temperature, Identity location
-        self.register_intent(intent, self.handle_lighting_intent)
+    def __build_switch_intent(self):
+        intent = IntentBuilder("switchIntent").require("SwitchActionKeyword") \
+            .require("Action").require("Entity").build()
+        self.register_intent(intent, self.handle_switch_intent)
+
+    def __build_light_set_intent(self):
+        intent = IntentBuilder("LightSetBrightnessIntent").optionally("LightsKeyword") \
+            .require("SetVerb") \
+            .require("Entity").require("BrightnessValue").build()
+        self.register_intent(intent, self.handle_light_set_intent)
+
+    def __build_light_adjust_intent(self):
+        intent = IntentBuilder("LightAdjBrightnessIntent").optionally("LightsKeyword") \
+            .one_of("IncreaseVerb", "DecreaseVerb", "LightBrightenVerb", "LightDimVerb") \
+            .require("Entity").optionally("BrightnessValue").build()
+        self.register_intent(intent, self.handle_light_adjust_intent)
 
     def __build_automation_intent(self):
         intent = IntentBuilder("AutomationIntent").require(
@@ -137,7 +156,8 @@ class HomeAssistantSkill(MycroftSkill):
         # TODO - Identity location, proximity
         self.register_intent(intent, self.handle_tracker_intent)
 
-    def handle_lighting_intent(self, message):
+    def handle_switch_intent(self, message):
+        LOGGER.debug("Starting Switch Intent")
         entity = message.data["Entity"]
         action = message.data["Action"]
         LOGGER.debug("Entity: %s" % entity)
@@ -148,6 +168,7 @@ class HomeAssistantSkill(MycroftSkill):
             self.speak_dialog('homeassistant.device.unknown', data={
                               "dev_name": ha_entity['dev_name']})
             return
+        LOGGER.debug("Entity State: %s" % ha_entity['state'])
         ha_data = {'entity_id': ha_entity['id']}
 
         if self.language == 'de':
@@ -155,31 +176,80 @@ class HomeAssistantSkill(MycroftSkill):
                 action = 'on'
             elif action == 'aus':
                 action = 'off'
-            elif action == 'runter'or action == 'dunkler':
+        if ha_entity['state'] == action:
+            LOGGER.debug("Entity in requested state")
+            self.speak_dialog('homeassistant.device.already', data={
+                "dev_name": ha_entity['dev_name'], 'action': action})
+        elif action in ["on", "off"]:
+            self.speak_dialog('homeassistant.device.%s' % action,
+                              data=ha_entity)
+            self.ha.execute_service("homeassistant", "turn_%s" % action,
+                                    ha_data)
+        else:
+            self.speak_dialog('homeassistant.error.sorry')
+
+    def handle_light_set_intent(self, message):
+        entity = message.data["Entity"]
+        try:
+            brightness_req = float(message.data["BrightnessValue"])
+            if brightness_req > 100 or brightness_req < 0:
+                self.speak_dialog('homeassistant.brightness.badreq')
+        except KeyError:
+            brightness_req = 10.0
+        brightness_value = int(brightness_req / 100 * 255)
+        brightness_percentage = int(brightness_req)
+        LOGGER.debug("Entity: %s" % entity)
+        LOGGER.debug("Brightness Value: %s" % brightness_value)
+        LOGGER.debug("Brightness Percent: %s" % brightness_percentage)
+        ha_entity = self.ha.find_entity(
+            entity, ['group', 'light'])
+        if ha_entity is None:
+            self.speak_dialog('homeassistant.device.unknown', data={
+                              "dev_name": ha_entity['dev_name']})
+            return
+        ha_data = {'entity_id': ha_entity['id']}
+        # TODO - Allow value set
+        if "SetVerb" in message.data:
+            ha_data['brightness'] = brightness_value
+            self.ha.execute_service("homeassistant", "turn_on", ha_data)
+            if self.language == 'de':
+                # TODO - Fix translation
+                self.speak("%s wurde gedimmt" % ha_entity['dev_name'])
+            else:
+                self.speak("Set the %s brightness to %s percent" %
+                           (ha_entity['dev_name'], brightness_percentage))
+        else:
+            self.speak_dialog('homeassistant.error.sorry')
+
+    def handle_light_adjust_intent(self, message):
+        entity = message.data["Entity"]
+        try:
+            brightness_req = float(message.data["BrightnessValue"])
+            if brightness_req > 100 or brightness_req < 0:
+                self.speak_dialog('homeassistant.brightness.badreq')
+        except KeyError:
+            brightness_req = 10.0
+        brightness_value = int(brightness_req / 100 * 255)
+        brightness_percentage = int(brightness_req)
+        LOGGER.debug("Entity: %s" % entity)
+        LOGGER.debug("Brightness Value: %s" % brightness_value)
+        ha_entity = self.ha.find_entity(
+            entity, ['group', 'light'])
+        if ha_entity is None:
+            self.speak_dialog('homeassistant.device.unknown', data={
+                              "dev_name": ha_entity['dev_name']})
+            return
+        ha_data = {'entity_id': ha_entity['id']}
+
+        if self.language == 'de':
+            if action == 'runter'or action == 'dunkler':
                 action = 'dim'
             elif action == 'heller' or action == 'hell':
                 action = 'brighten'
-        if action == "on":
-            if ha_entity['state'] == action:
-                self.speak_dialog(
-                    'homeassistant.device.already', data={
-                        "dev_name": ha_entity['dev_name'], 'action': action})
-            else:
-                self.speak_dialog('homeassistant.device.on', data=ha_entity)
-                self.ha.execute_service("homeassistant", "turn_on", ha_data)
-        elif action == "off":
-            if ha_entity['state'] == action:
-                self.speak_dialog(
-                    'homeassistant.device.already', data={
-                        "dev_name": ha_entity['dev_name'], 'action': action})
-            else:
-                self.speak_dialog('homeassistant.device.off', data=ha_entity)
-                self.ha.execute_service("homeassistant", "turn_off", ha_data)
         # TODO - Allow Dimming
-        elif action == "dim":
+        if "DecreaseVerb" in message.data or \
+                "LightDimVerb" in message.data:
             if ha_entity['state'] == "off":
-                self.speak_dialog('homeassistant.device.off', data={
-                                  "dev_name": ha_entity['dev_name']})
                 if self.language == 'de':
                     self.speak("Kann %s nicht dimmen. Es ist aus." %
                                ha_entity['dev_name'])
@@ -187,20 +257,22 @@ class HomeAssistantSkill(MycroftSkill):
                     self.speak("Can not dim %s. It is off." %
                                ha_entity['dev_name'])
             else:
-                if ha_entity['attributes']['brightness'] < 25:
+                light_attrs = self.ha.find_entity_attr(ha_entity['id'])
+                ha_data['brightness'] = light_attrs[0]
+                if ha_data['brightness'] < brightness_value:
                     ha_data['brightness'] = 10
                 else:
-                    ha_data['brightness'] -= 25
+                    ha_data['brightness'] -= brightness_value
                 self.ha.execute_service("homeassistant", "turn_on", ha_data)
                 if self.language == 'de':
                     self.speak("%s wurde gedimmt" % ha_entity['dev_name'])
                 else:
                     self.speak("Dimmed the %s" % ha_entity['dev_name'])
         # TODO - Allow Brightening
-        elif action == "brighten":
+        # elif action == "brighten":
+        elif "IncreaseVerb" in message.data or \
+                "LightBrightenVerb" in message.data:
             if ha_entity['state'] == "off":
-                self.speak_dialog('homeassistant.device.off', data={
-                                  "dev_name": ha_entity['dev_name']})
                 if self.language == 'de':
                     self.speak("Kann %s nicht dimmen. Es ist aus." %
                                ha_entity['dev_name'])
@@ -208,10 +280,12 @@ class HomeAssistantSkill(MycroftSkill):
                     self.speak("Can not dim %s. It is off." %
                                ha_entity['dev_name'])
             else:
-                if ha_entity['attributes']['brightness'] > 230:
+                light_attrs = self.ha.find_entity_attr(ha_entity['id'])
+                ha_data['brightness'] = light_attrs[0]
+                if ha_data['brightness'] > brightness_value:
                     ha_data['brightness'] = 255
                 else:
-                    ha_data['brightness'] += 25
+                    ha_data['brightness'] += brightness_value
                 self.ha.execute_service("homeassistant", "turn_on", ha_data)
                 if self.language == 'de':
                     self.speak("Erhoehe helligkeit auf %s" %
